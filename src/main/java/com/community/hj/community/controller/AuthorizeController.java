90f48a2178feb3bd32ce01b2a273cb9ced8bc701
ainstinct <1126056803@qq.com>
2020-07-25 21:27:05 +0800

    add flyway

--- src/main/java/com/community/hj/community/controller/AuthorizeController.java
+++ src/main/java/com/community/hj/community/controller/AuthorizeController.java
@@ -2,12 +2,19 @@ package com.community.hj.community.controller;
 
 import com.community.hj.community.dto.AccessTokenDTO;
 import com.community.hj.community.dto.GithubUser;
+import com.community.hj.community.mapper.UserMapper;
+import com.community.hj.community.model.User;
 import com.community.hj.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
@@ -22,9 +29,14 @@ public class AuthorizeController {
     @Value("${github.redirect}")
     private String redirectUri;
 
+    @Autowired
+    private UserMapper userMapper;
 
     @GetMapping("/callback")
-    public String callback(@RequestParam(name="code") String code, @RequestParam(name="state") String state){//接受参数
+    public String callback(@RequestParam(name="code") String code,
+                           @RequestParam(name="state") String state,
+                           HttpServletRequest request,
+                           HttpServletResponse response){//接受参数
 
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
@@ -33,8 +45,47 @@ public class AuthorizeController {
         accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);//ctrl+alt+v
-        GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        GithubUser githubUser = githubProvider.getUser(accessToken);//使用github登录成功以后
+        if(githubUser != null){
+            User user = new User();
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            response.addCookie(new Cookie("token",token));
+
+            //登录成功，写cookie和session
+            //request.getSession().setAttribute("user",githubUser);//说明session银行账户创建成功,框架自动签发一个银行卡
+            return "redirect:/";
+        }else{
+            //登录失败，重新登录
+            return "redirect:/";
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/com/community/hj/community/controller/IndexController.java
+++ src/main/java/com/community/hj/community/controller/IndexController.java
@@ -1,14 +1,52 @@
 package com.community.hj.community.controller;
 
+import com.community.hj.community.mapper.UserMapper;
+import com.community.hj.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+
 @Controller
 public class IndexController {
+    @Autowired
+    private UserMapper userMapper;
+
+    /*
+    当访问首页的时候，循环去看所有的cookies，找到cookies=“token”的，拿到这个cookies去数据库里查，是不是有这个记录
+    如果有将放到session中(request.getsession.setAattribute....),前端通过session来判断是否展示“我”或“登录”
+
+    */
     @GetMapping("/")
-    public String index(){
+    public String index(HttpServletRequest request){
+        Cookie[] cookies = request.getCookies();
+        if(cookies != null){
+            for(Cookie cookie : cookies){
+                if(cookie.getName().equals("token")){
+                    String token = cookie.getValue();
+                    User user = userMapper.findByToken(token);
+                    if(user != null){
+                        request.getSession().setAttribute("user",user);
+                    }
+                    break;
+                }
+            }
+        }
         return "index";
     }
 }
+
+
+
+
+
+
+
+
+
+
